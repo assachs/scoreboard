4a361d2c00e11c3b966609dabdb631e3b518cae7
andi <a.sachs@vsd.volley.de>
2025-02-22 15:20:46 +0100

    V2025.01

--- files/install.sh
+++ files/install.sh
@@ -9,7 +9,7 @@ sudo cp /home/pi/scoreboard/files/scoreboard.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable scoreboard.service
 
-rm -fr /home/pi/stomp_ws_py
+sudo rm -fr /home/pi/stomp_ws_py
 git clone https://github.com/assachs/stomp_ws_py.git
 sudo apt install fonts-freefont-otf python3-websocket otf2bdf
 
